3879dea75584a39cc8eebc3325802414a51e4d7c
Hamza Talat <hamzacap996@gmail.com>
2020-07-24 03:21:13 +0500

    added the functionality of drawing rectangle

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {cx: '' ,cy:'' , r:'' , ty:'' ,statement:''};
+    this.state = {type:'' ,statement:'' , 
+                  cx: '' ,cy:'' , cr:'',
+                  rect_x_axis: '' ,rect_y_axis:'' ,rect_height:'' , rect_width:''
+    };
     this.handleChange = this.handleChange.bind(this);
     this.activate_draw = this.activate_draw.bind(this);
   }
@@ -29,28 +32,68 @@ handleChange(event) {
 
   const answer_array = this.state.statement.split(" ");
   if(answer_array[1]){
-    this.setState({ ty: answer_array[0] });
+    this.setState({ type: answer_array[0] });
     answer_array[0]=''
   }
   
 
   //  Work flow for Circle 
     // Setting X axis of the circle 
-  if(answer_array[2] && this.state.ty =='c'){
-    this.setState({ cx: answer_array[1] });
-    answer_array[1]=''
+  if(answer_array[2] ){
+    if(this.state.type ==='c')
+     {
+      this.setState({ cx: answer_array[1] });
+      answer_array[1]=''
+     }
+
+     if(this.state.type ==='r')
+     {
+      this.setState({ rect_x_axis: answer_array[1] });
+      answer_array[1]=''
+     }
+
   }
 
     // Setting y axis of the circle 
-  if(answer_array[3] && this.state.ty =='c' ){
-    this.setState({ cy: answer_array[2] });
-    answer_array[2]=''
+  if(answer_array[3] ){
+    if(this.state.type ==='c')
+     {
+      this.setState({ cy: answer_array[2] });
+      answer_array[2]=''
+     }
+     if(this.state.type ==='r')
+     {
+      this.setState({ rect_y_axis: answer_array[2] });
+      answer_array[2]=''
+     }
   }
 
     //Setting up Radius of the circle  
-  if(!answer_array[4] && this.state.ty =='c'){
-    this.setState({ r: answer_array[3] });
-    answer_array[3]=''
+  if(!answer_array[4]){
+    if(this.state.type ==='c')
+     {
+      this.setState({ cr: answer_array[3] });
+      answer_array[3]=''
+     }
+     if(this.state.type ==='r')
+     {
+      this.setState({ rect_height: answer_array[3] });
+      answer_array[3]=''
+     }
+  }
+
+
+  if(!answer_array[5]){
+    // if(this.state.type ==='c')
+    //  {
+    //   this.setState({ cr: answer_array[3] });
+    //   answer_array[3]=''
+    //  }
+     if(this.state.type ==='r')
+     {
+      this.setState({ rect_width: answer_array[4] });
+      answer_array[4]=''
+     }
   }
   
 
@@ -62,26 +105,24 @@ activate_draw(event)
 }
 
   render() {
-  return (
-    <div>
-      <form>
-        <textarea
-          value={this.state.textAreaValue}
-          onChange={this.handleChange}
-        />
-        <button onClick={this.activate_draw}> Draw</button>
-      </form>
+    return (
+      <div>
+        <form>
+          <textarea
+            value={this.state.textAreaValue}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.activate_draw}> Draw</button>
+        </form>
 
-       <svg width="100" height="100">
-              <circle cx= {this.state.cx} cy= {this.state.cy} r={this.state.r} stroke="green" stroke-width="4" fill={this.state.ty} />
-          </svg>
-        <div>
-          {this.state.statement}
+        <svg width="250px" height="250px">
+          <circle cx= {this.state.cx} cy= {this.state.cy} r={this.state.cr} stroke="green" stroke-width="4" fill='red' />
+        
+          <rect x={this.state.rect_x_axis} y={this.state.rect_x_axis}  width={this.state.rect_width} height={this.state.rect_height} stroke="green" stroke-width="4" fill='red'/>
+        </svg>
 
-
-        </div>
-    </div>
-  );
+      </div>
+    );
   }
 }
 
